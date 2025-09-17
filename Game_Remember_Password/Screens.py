e51fa29b4693244e3f7f6d8bef6a182691c3f22d
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2025-09-17 11:54:15 +0200

    basic layout for the game screen

--- Game_Remember_Password/Screens.py
+++ Game_Remember_Password/Screens.py
@@ -29,10 +29,15 @@ class InitialScreen(MyScreen):
         MyScreen.__init__(self, parent, controller)
         self._password: str = ""
 
-        tk.Label(self, text="Enter the password to train").grid(column=0, row=0, sticky="nwe")
+        tk.Label(self, text="Enter the password to train").grid(column=0, row=0, pady=20, sticky="nwe")
         self._password_entry: tk.Entry = tk.Entry(self, width=40, show="*")
-        self._password_entry.grid(column=0, row=1)
-        tk.Button(self, text="Train", command=self._trainCallback).grid(column=0, row=2)
+        self._password_entry.grid(column=0, row=1, pady=20)
+        tk.Button(self, text="Train", command=self._trainCallback).grid(column=0, row=2, pady=20)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
 
     def _trainCallback(self) -> None:
         self._password = self._password_entry.get()
@@ -49,10 +54,25 @@ class GameScreen(MyScreen):
     def __init__(self, parent: tk.Frame, controller: "Game") -> None:
         MyScreen.__init__(self, parent, controller)
 
-        tk.Button(self, text="Start again", command=self._backCallback).grid(column=0, row=0)
+        self._entry: tk.Entry = tk.Entry(self, width=40)
+        self._entry.grid(column=0, row=0, pady=20)
+        tk.Button(self, text="Check", command=self._checkCallback).grid(column=0, row=1, pady=20)
+        self._display: tk.Text = tk.Text(self, width=40, height=1, state="disabled")
+        self._display.grid(column=0, row=2, pady=20)
+        tk.Button(self, text="Start again", command=self._backCallback).grid(column=0, row=3, pady=20)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_rowconfigure(3, weight=1)
 
     def _backCallback(self) -> None:
         self.controller.showScreen(Screens.INITIAL)
 
+    def _checkCallback(self) -> None:
+        raise NotImplementedError
+
     def setScreen(self) -> None:
-        pass
+        self._entry.delete(0, tk.END)
+        self._display.delete("1.0", tk.END)
