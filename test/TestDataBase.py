fd2b830bb5ef41d4aa1f6957cb4b5f5723ffa8d4
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2025-11-04 14:53:48 +0100

    Test for create and delete entries
    Still need to implement

--- test/TestDataBase.py
+++ test/TestDataBase.py
@@ -9,6 +9,16 @@ from Game_Remember_Password.Database import DataBase
 class DataBaseTest(unittest.TestCase):
     """Test Case for DataBase"""
 
+    def tearDown(self) -> None:
+        folder: str = "test/data/"
+        files: list[str] = ["create_file.db", "createDB.db", "create_delete.db"]
+        for file in files:
+            path: str = folder + file
+            if os.path.exists(path):
+                os.remove(path)
+
+        open(folder + "createDB.db", "w", encoding="utf_8").close()
+
     def testCreateFile(self) -> None:
         """Test the creation of the database file"""
         file: str = "test/data/create_file.db"
@@ -21,21 +31,30 @@ class DataBaseTest(unittest.TestCase):
             pass
         self.assertTrue(os.path.exists(file))
 
-        os.remove(file)
-        self.assertFalse(os.path.exists(file))
-
     def testCreateNewDB(self) -> None:
         """Test the creation of a database"""
         file: str = "test/data/createDB.db"
         with DataBase(file) as db:
             self.assertFalse(db.getEntries())
 
-        os.remove(file)
-        open(file, "w", encoding="utf_8").close()
-
     def testGetEntries(self) -> None:
-        """Thest DB get entries"""
+        """Test DB get entries"""
         file: str = "test/data/get.db"
         with DataBase(file) as db:
             entries: list[tuple[str, str]] = db.getEntries()
             self.assertEqual(entries, [("test", "password"), ("test2", "password2")])
+
+    def testCreateAndDelete(self) -> None:
+        """Test that the DataBase can create and delete entries"""
+        file: str = "test/data/create_delete.db"
+        with DataBase(file) as db:
+            self.assertFalse(db.getEntries())
+            db.createEntry("test", "password")
+            db.createEntry("test2", "password2")
+            self.assertEqual(db.getEntries(), [("test", "password"), ("test2", "password2")])
+            db.createEntry("test2", "password2")
+            self.assertEqual(db.getEntries(), [("test", "password"), ("test2", "password2")])
+            db.deleteEntry("test")
+            self.assertEqual(db.getEntries(), [("test2", "password2")])
+            db.deleteEntry("test")
+            self.assertEqual(db.getEntries(), [("test2", "password2")])
